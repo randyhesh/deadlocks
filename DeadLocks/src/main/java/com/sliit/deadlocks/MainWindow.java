d6488763cc801aa88e20e2f3067d0597d12ec7b3
randyhesh <heshani7.herath@gmail.com>
2016-08-12 00:16:23 +0530

    commit

---
+++ DeadLocks/src/main/java/com/sliit/deadlocks/MainWindow.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sliit.deadlocks;
+
+import com.sliit.views.Home;
+
+/**
+ *
+ * @author Heshani
+ */
+public class MainWindow {
+ 
+    public static void main(String[] args) {
+        Home home=new Home();
+        home.setVisible(true);
+    }
+}
